f7b6a87d4829cef7db92489cecd8a3963c1418bf
Siemenstechnicalacademy <hiteshchaubey27@gmail.com>
2026-04-02 19:19:44 +0530

    Add files via upload

--- students_db.js
+++ students_db.js
@@ -4,13 +4,10 @@ const students = [
     // ===================== 61st BATCH =====================
 
     // --- 61st EA (Division A) ---
-    { name: "Tejas Ganpat Padwal",          pass: "3311", batch: "61st", division: "EA" },
-    { name: "Tanuja Walimbe",               pass: "3346", batch: "61st", division: "EA" },
-    { name: "Sankit kahar",                 pass: "3284", batch: "61st", division: "EA" },
-    { name: "Nayan kondilkar",              pass: "3291", batch: "61st", division: "EA" },
-    { name: "Om vishe",                     pass: "3342", batch: "61st", division: "EA" },
-    { name: "Hitesh",                       pass: "2685", batch: "61st", division: "EA" },
-    { name: "Deva",                         pass: "1371", batch: "61st", division: "EA" },
+    { name: "Hitesh",                      pass: "2685", batch: "61st", division: "EA" },
+    { name: "Deva",                        pass: "1371", batch: "61st", division: "EA" },
+    { name: "Tejas Ganpat Padwal",         pass: "3311", batch: "61st", division: "EA" },
+    { name: "Tanuja Suresh Shirke",         pass: "3346", batch: "61st", division: "EA" },
     { name: "Bhushan Gurunath Vishe",       pass: "3341", batch: "61st", division: "EA" },
     { name: "Yash Ankush Kumawat",          pass: "3294", batch: "61st", division: "EA" },
     { name: "Yadnyesh Prakash Partole",     pass: "3313", batch: "61st", division: "EA" },
@@ -36,12 +33,15 @@ const students = [
     { name: "Pramita Gautam Vavhal",        pass: "3340", batch: "61st", division: "EA" },
     { name: "Ruchita Pandurang Farde",      pass: "3271", batch: "61st", division: "EA" },
     { name: "Dimpal Rasal",                 pass: "3321", batch: "61st", division: "EA" },
+    { name: "Tanuja Walimbe",               pass: "3346", batch: "61st", division: "EA" },
+    { name: "Sankit Kahar",                 pass: "3284", batch: "61st", division: "EA" },
+    { name: "Nayan Kondilkar",              pass: "3291", batch: "61st", division: "EA" },
+    { name: "Om Vishe",                     pass: "3342", batch: "61st", division: "EA" },
 
     // --- 61st EB (Division B) ---
+    { name: "Hitesh",                      pass: "2685", batch: "61st", division: "EB" },
+    { name: "Deva",                        pass: "1371", batch: "61st", division: "EB" },
     { name: "Jayshree Vasant Dongare",      pass: "3352", batch: "61st", division: "EB" },
-    { name: "Shivam Thakur",                pass: "3337", batch: "61st", division: "EB" },
-    { name: "Hitesh",                       pass: "2685", batch: "61st", division: "EB" },
-    { name: "Deva",                         pass: "1371", batch: "61st", division: "EB" },
     { name: "Sayali Mahadev Gondhali",      pass: "3276", batch: "61st", division: "EB" },
     { name: "Kartiki Kadam",                pass: "3351", batch: "61st", division: "EB" },
     { name: "Nishant Bharat Jadhav",        pass: "3289", batch: "61st", division: "EB" },
@@ -69,13 +69,14 @@ const students = [
     { name: "Aryan Anil Shinde",            pass: "3328", batch: "61st", division: "EB" },
     { name: "Nikhil Pradip Jadhav",         pass: "3285", batch: "61st", division: "EB" },
     { name: "Purva Panchal",                pass: "3356", batch: "61st", division: "EB" },
+    { name: "Shivam Thakur",                pass: "3337", batch: "61st", division: "EB" },
 
     // ===================== 62nd BATCH =====================
 
     // --- 62nd EA (Division A) ---
+    { name: "Hitesh",                      pass: "2685", batch: "62nd", division: "EA" },
+    { name: "Deva",                        pass: "1371", batch: "62nd", division: "EA" },
     { name: "Yashraj Dinesh Patil",         pass: "3472", batch: "62nd", division: "EA" },
-    { name: "Hitesh",                       pass: "2685", batch: "62nd", division: "EA" },
-    { name: "Deva",                         pass: "1371", batch: "62nd", division: "EA" },
     { name: "Velvin Vishal Gupta",           pass: "3466", batch: "62nd", division: "EA" },
     { name: "Jagruti Madhukar Patil",        pass: "3402", batch: "62nd", division: "EA" },
     { name: "Harshad Keshav Bajage",         pass: "3398", batch: "62nd", division: "EA" },
@@ -111,9 +112,9 @@ const students = [
     { name: "Chirag Viraj Patil",            pass: "3385", batch: "62nd", division: "EA" },
 
     // --- 62nd EB (Division B) ---
+    { name: "Hitesh",                      pass: "2685", batch: "62nd", division: "EB" },
+    { name: "Deva",                        pass: "1371", batch: "62nd", division: "EB" },
     { name: "Gayatri Balakrishna Bhoir",     pass: "3396", batch: "62nd", division: "EB" },
-    { name: "Hitesh",                        pass: "2685", batch: "62nd", division: "EB" },
-    { name: "Deva",                          pass: "1371", batch: "62nd", division: "EB" },
     { name: "Vaishnavi Sitaram Gaikwad",     pass: "3461", batch: "62nd", division: "EB" },
     { name: "Kunal Narayan Padwal",          pass: "3412", batch: "62nd", division: "EB" },
     { name: "Bhagyashree Sachin Ghude",      pass: "3379", batch: "62nd", division: "EB" },
